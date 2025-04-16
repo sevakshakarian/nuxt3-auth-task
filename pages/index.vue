a0bce04dcb92ced925636832024dbc19b7458da8
MAC <mac@MacBook-Pro-MAC.local>
2025-04-16 12:11:58 -0700

    feat(auth): add register and forgot-password links to login page

--- pages/index.vue
+++ pages/index.vue
@@ -37,8 +37,8 @@
         </p>
       </UForm>
       <div class="flex justify-between text-sm mt-4">
-        <NuxtLink to="/register" class="text-blue-600 hover:underline">Регистрация</NuxtLink>
-        <NuxtLink to="/forgot-password" class="text-blue-600 hover:underline">Забыли пароль?</NuxtLink>
+        <NuxtLink to="/" class="text-blue-600 hover:underline">Регистрация</NuxtLink>
+        <NuxtLink to="/" class="text-blue-600 hover:underline">Забыли пароль?</NuxtLink>
       </div>
     </div>
   </div>
